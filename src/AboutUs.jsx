d4d98b597d926f7da9e0456bf9bc145004d4935a
TishaMdr58 <mdrtisha@gmail.com>
2024-01-11 12:41:09 +0545

    jina

---
+++ src/AboutUs.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <>
+      <div className="container">
+        <div className="main-container">
+          <div className="image">
+            <img src="../public/jinalch.png" alt="" />
+          </div>
+          <div className="paragraph">
+            <div className="flower">
+              <img className="flower" src="../public/Group.svg" alt="" />
+            </div>
+            <h1 className="header">Our story</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              consequat diam non lorem rhoncus ultrices. Nunc id quam ut arcu
+              bibendum faucibus at at arcu. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Nunc consequat diam non lorem rhoncus
+              ultrices. Nunc id quam ut arcu bibendum faucibus at at arcu. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat
+              diam non lorem rhoncus ultrices. Nunc id quam ut arcu bibendum
+              faucibus at at arcu. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Nunc consequat diam non lorem rhoncus ultrices.
+              Nunc id quam ut arcu bibendum faucibus at at arcu.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutUs;
